7e5cb40471feb6f0ae3ec240c1316a6e340cf1f3
drw <drw@users.noreply.github.com>
2021-02-15 13:28:12 -0500

    Move time-range-determining functions to their own framework-level util file

--- engine/payload/united_way/211.py
+++ engine/payload/united_way/211.py
@@ -1,4 +1,4 @@
-import csv, json, requests, sys, traceback
+import csv, requests, sys, traceback
 from datetime import datetime, timedelta
 from dateutil import parser
 from pprint import pprint
@@ -8,6 +8,7 @@ from collections import OrderedDict
 from marshmallow import fields, pre_load, post_load
 from engine.wprdc_etl import pipeline as pl
 from engine.etl_util import fetch_city_file, write_to_csv
+from engine.time_field_util import get_extant_time_range
 from engine.notify import send_to_slack
 from engine.parameters.remote_parameters import TEST_PACKAGE_ID
 
@@ -214,160 +215,6 @@ def decode_calls(calls):
         call['needs_code'] = call.get('TaxCode', None)
         call['date_of_call'] = call.get('Date_of_Call', None)
     return calls
-
-def query_resource(site,query,API_key=None):
-    # Use the datastore_search_sql API endpoint to query a CKAN resource.
-
-    # Note that this doesn't work for private datasets.
-    # The relevant CKAN GitHub issue has been closed.
-    # https://github.com/ckan/ckan/issues/1954
-    ckan = ckanapi.RemoteCKAN(site, apikey=API_key)
-    response = ckan.action.datastore_search_sql(sql=query)
-    # A typical response is a dictionary like this
-    #{u'fields': [{u'id': u'_id', u'type': u'int4'},
-    #             {u'id': u'_full_text', u'type': u'tsvector'},
-    #             {u'id': u'pin', u'type': u'text'},
-    #             {u'id': u'number', u'type': u'int4'},
-    #             {u'id': u'total_amount', u'type': u'float8'}],
-    # u'records': [{u'_full_text': u"'0001b00010000000':1 '11':2 '13585.47':3",
-    #               u'_id': 1,
-    #               u'number': 11,
-    #               u'pin': u'0001B00010000000',
-    #               u'total_amount': 13585.47},
-    #              {u'_full_text': u"'0001c00058000000':3 '2':2 '7827.64':1",
-    #               u'_id': 2,
-    #               u'number': 2,
-    #               u'pin': u'0001C00058000000',
-    #               u'total_amount': 7827.64},
-    #              {u'_full_text': u"'0001c01661006700':3 '1':1 '3233.59':2",
-    #               u'_id': 3,
-    #               u'number': 1,
-    #               u'pin': u'0001C01661006700',
-    #               u'total_amount': 3233.59}]
-    # u'sql': u'SELECT * FROM "d1e80180-5b2e-4dab-8ec3-be621628649e" LIMIT 3'}
-    data = response['records']
-    return data
-
-def random_string(stringLength=10):
-    """Generate a random string of fixed length."""
-    import random, string
-    letters = string.ascii_lowercase
-    return ''.join(random.choice(letters) for i in range(stringLength))
-
-def find_extremes(resource_id, field):
-    from credentials import site, ckan_api_key as API_key
-
-    biggest_name = 'biggest_' + random_string(5) # Append a random string to avoid query caching.
-    query = 'SELECT min("{}") AS smallest, max("{}") as {} FROM "{}" LIMIT 1'.format(field,field,biggest_name,resource_id)
-    record = query_resource(site=site, query=query, API_key=API_key)[0]
-    return record['smallest'], record[biggest_name]
-
-def get_metadata(site, resource_id, API_key=None):
-    metadata = ckan.action.resource_show(id=resource_id)
-
-    return metadata
-
-def get_resource_parameter(site, resource_id, parameter=None, API_key=None):
-    """Gets a CKAN resource parameter. If no parameter is specified, all metadata
-    for that resource is returned."""
-    # Some resource parameters you can fetch with this function are
-    # 'cache_last_updated', 'package_id', 'webstore_last_updated',
-    # 'datastore_active', 'id', 'size', 'state', 'hash',
-    # 'description', 'format', 'last_modified', 'url_type',
-    # 'mimetype', 'cache_url', 'name', 'created', 'url',
-    # 'webstore_url', 'mimetype_inner', 'position',
-    # 'revision_id', 'resource_type'
-    # Note that 'size' does not seem to be defined for tabular
-    # data on WPRDC.org. (It's not the number of rows in the resource.)
-    try:
-        ckan = ckanapi.RemoteCKAN(site, apikey=API_key)
-        metadata = get_metadata(ckan, resource_id, API_key)
-        if parameter is None:
-            return metadata
-        else:
-            return metadata[parameter]
-    except:
-        raise RuntimeError("Unable to obtain resource parameter '{}' for resource with ID {}".format(parameter,resource_id))
-
-def find_extreme_dates(resource_id, time_field_lookup):
-    from credentials import site, ckan_api_key as API_key
-
-    datastore_active = get_resource_parameter(site, resource_id, 'datastore_active', API_key)
-    if datastore_active:
-        if resource_id in time_field_lookup:
-            time_field = time_field_lookup[resource_id]
-            if time_field is not None:
-                first, last = find_extremes(resource_id, time_field)
-                first = parser.parse(first)
-                last = parser.parse(last)
-                return first, last
-    return None, None
-
-#def synthesize_pseudo_time_field_lookup(job):
-
-def get_extant_time_range(job, **kwparameters):
-    if 'ckan' in job.destinations: # This is a strong argument for making each job_dict
-        # have exactly one source and one destination and using job molecules or
-        # chains to support multiple destinations (somehow).
-        ## JOB CHAINS: 1) Support building up more complicated processes (often 
-        ## represented by "directed acyclic graphs") by chaining job atoms.
-        ## 2) More coherently support multiple destinations at this level by
-        ## calling the same job atom twice, with different destinations.
-        ## 3) But try to make every parameter into a potential list this way
-        ## (at least by allowing one parameter at a time to be changed).
-
-        package = job.package
-        if 'extras' in package:
-            extras_list = package['extras']
-            # Keep definitions and uses of extras metadata updated here:
-            # https://github.com/WPRDC/data-guide/blob/master/docs/metadata_extras.md
-            # The format is like this:
-            #       u'extras': [{u'key': u'dcat_issued', u'value': u'2014-01-07T15:27:45.000Z'}, ...
-            # not a dict, but a list of dicts.
-            extras = {d['key']: d['value'] for d in extras_list}
-            #if 'dcat_issued' not in extras:
-            if 'time_field' in extras:
-                time_field_lookup = json.loads(extras['time_field'])
-                first_date, last_date = find_extreme_dates(job.resource_id, time_field_lookup)
-                return first_date, last_date
-            else:
-                try:
-                    time_field = getattr(job, 'time_field', None) # Try to grab time_field from job
-                except AttributeError:
-                    return None, None
-                else:
-                    time_field_lookup = {job.resource_id: time_field}
-                    first_date, last_date = find_extreme_dates(job.resource_id, time_field_lookup)
-                    return first_date, last_date
-                return None, None
-        else:
-            return None, None
-    else: # Find the time range of a non-datastore CSV file at one of the actual destinations.
-            # OR wipe the existing file and rewrite it from scratch.
-        if 'file' in job.destinations:
-            try:
-                f = open(job.destination_file_path, 'r')
-            except FileNotFoundError:
-                return None, None
-            else:
-                reader = csv.DictReader(f)
-                first_date = datetime.max
-                last_date = datetime.min
-                for row in reader:
-                    timestamp = row.get(job.time_field, None)
-                    if timestamp is not None:
-                        timestamp = parser.parse(timestamp)
-                        if timestamp < first_date:
-                            first_date = timestamp
-                        if timestamp > last_date:
-                            last_date = timestamp
-                f.close()
-                if first_date <= last_date:
-                    return first_date, last_date
-                else:
-                    return None, None
-        else:
-            raise ValueError(f"Unable to determine the extant time range for the following destinations: {job.destinations}")
 
 def pull_211_requests_and_save_to_file(job, **kwparameters):
     """Examine the extant resource to find the last date included in the
---
+++ engine/time_field_util.py
@@ -0,0 +1,108 @@
+"""Functions for determining the time range of records in a CKAN datastore or CSV file."""
+import csv, json
+from datetime import datetime, timedelta
+from dateutil import parser
+from pprint import pprint
+
+from engine.etl_util import query_resource, get_resource_parameter
+
+try:
+    from icecream import ic
+except ImportError:  # Graceful fallback if IceCream isn't installed.
+    ic = lambda *a: None if not a else (a[0] if len(a) == 1 else a)  # noqa
+
+def random_string(stringLength=10):
+    """Generate a random string of fixed length."""
+    import random, string
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(stringLength))
+
+def find_extremes(resource_id, field):
+    from credentials import site, ckan_api_key as API_key
+
+    biggest_name = 'biggest_' + random_string(5) # Append a random string to avoid query caching.
+    query = 'SELECT min("{}") AS smallest, max("{}") as {} FROM "{}" LIMIT 1'.format(field,field,biggest_name,resource_id)
+    record = query_resource(site=site, query=query, API_key=API_key)[0] # Note that query_resource doesn't
+    # work for private datasets. Therefore it won't be possible to test the time_field aspects on a private dataset,
+    # including partial filling from the last date of the dataset to the present date.
+    return record['smallest'], record[biggest_name]
+
+def find_extreme_dates(resource_id, time_field_lookup):
+    from credentials import site, ckan_api_key as API_key
+
+    datastore_active = get_resource_parameter(site, resource_id, 'datastore_active', API_key)
+    if datastore_active:
+        if resource_id in time_field_lookup:
+            time_field = time_field_lookup[resource_id]
+            if time_field is not None:
+                first, last = find_extremes(resource_id, time_field)
+                first = parser.parse(first)
+                last = parser.parse(last)
+                return first, last
+    return None, None
+
+#def synthesize_pseudo_time_field_lookup(job):
+
+def get_extant_time_range(job, **kwparameters):
+    if 'ckan' in job.destinations: # This is a strong argument for making each job_dict
+        # have exactly one source and one destination and using job molecules or
+        # chains to support multiple destinations (somehow).
+        ## JOB CHAINS: 1) Support building up more complicated processes (often 
+        ## represented by "directed acyclic graphs") by chaining job atoms.
+        ## 2) More coherently support multiple destinations at this level by
+        ## calling the same job atom twice, with different destinations.
+        ## 3) But try to make every parameter into a potential list this way
+        ## (at least by allowing one parameter at a time to be changed).
+
+        package = job.package
+        if 'extras' in package:
+            extras_list = package['extras']
+            # Keep definitions and uses of extras metadata updated here:
+            # https://github.com/WPRDC/data-guide/blob/master/docs/metadata_extras.md
+            # The format is like this:
+            #       u'extras': [{u'key': u'dcat_issued', u'value': u'2014-01-07T15:27:45.000Z'}, ...
+            # not a dict, but a list of dicts.
+            extras = {d['key']: d['value'] for d in extras_list}
+            #if 'dcat_issued' not in extras:
+            if 'time_field' in extras:
+                time_field_lookup = json.loads(extras['time_field'])
+                first_date, last_date = find_extreme_dates(job.resource_id, time_field_lookup)
+                return first_date, last_date
+            else:
+                try:
+                    time_field = getattr(job, 'time_field', None) # Try to grab time_field from job
+                except AttributeError:
+                    return None, None
+                else:
+                    time_field_lookup = {job.resource_id: time_field}
+                    first_date, last_date = find_extreme_dates(job.resource_id, time_field_lookup)
+                    return first_date, last_date
+                return None, None
+        else:
+            return None, None
+    else: # Find the time range of a non-datastore CSV file at one of the actual destinations.
+            # OR wipe the existing file and rewrite it from scratch.
+        if 'file' in job.destinations:
+            try:
+                f = open(job.destination_file_path, 'r')
+            except FileNotFoundError:
+                return None, None
+            else:
+                reader = csv.DictReader(f)
+                first_date = datetime.max
+                last_date = datetime.min
+                for row in reader:
+                    timestamp = row.get(job.time_field, None)
+                    if timestamp is not None:
+                        timestamp = parser.parse(timestamp)
+                        if timestamp < first_date:
+                            first_date = timestamp
+                        if timestamp > last_date:
+                            last_date = timestamp
+                f.close()
+                if first_date <= last_date:
+                    return first_date, last_date
+                else:
+                    return None, None
+        else:
+            raise ValueError(f"Unable to determine the extant time range for the following destinations: {job.destinations}")
