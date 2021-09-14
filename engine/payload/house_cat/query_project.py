74a4cfccec070910c575f7d99290732e8364f622
drw <drw@users.noreply.github.com>
2021-09-14 10:56:03 -0400

    Fix house_cat query_project and add output with all fields, in order

--- engine/payload/house_cat/query_project.py
+++ engine/payload/house_cat/query_project.py
@@ -7,7 +7,7 @@ from collections import OrderedDict, defaultdict
 from marshmallow import fields, pre_load, post_load
 from engine.wprdc_etl import pipeline as pl
 from engine.etl_util import write_to_csv
-from engine.ckan_util import query_resource
+from engine.ckan_util import query_resource, get_resource_fields
 from engine.notify import send_to_slack
 from engine.parameters.local_parameters import SOURCE_DIR, PRODUCTION
 from engine.post_processors import check_for_empty_table
@@ -35,7 +35,7 @@ def find_all_linked_keys(id_field_name, id_field_value):
     return None
 
 def query_housing_project(id_field_name, id_field_value):
-    # Get dict of IDs ({'property_id': 'WHATEVER', ...}) by 
+    # Get dict of IDs ({'property_id': 'WHATEVER', ...}) by
     # combining user-entered field name and value (e.g., property_id = '803018501398')
     # with deduplicated_index.csv (which has ALL the project ID values).
     from engine.credentials import site, API_key
@@ -50,60 +50,75 @@ def query_housing_project(id_field_name, id_field_value):
     utilized_ids_fields = list(set([item for sublist in id_fields_by_code.values() for item in sublist]))
     id_fields_and_values = dict([(k,v) for k,v in id_fields_and_values.items() if k in utilized_ids_fields and v != ''])
 
-    records_by_job_code = {}
+    value_by_job_code_and_field = defaultdict(dict)
+    fields_by_job_code = {}
     rows_with_values = []
+
     with open('/Users/drw/WPRDC/etl/rocket-etl/output_files/house_cat/sources.csv') as g:
         reader = csv.DictReader(g)
         for source in reader:
-            ic(source['job_code'])
+            job_code = source['job_code']
+            ic(job_code)
             table_id_fields = source['id_fields'].split('|')
             ic(table_id_fields)
             list_of_dicts = []
             where_clauses = []
             resource_id = source['resource_id']
 
+            fields_by_job_code[job_code] = get_resource_fields(site, resource_id, API_key)[0]
+
             for id_f, id_value in id_fields_and_values.items():
                 if id_f in table_id_fields:
                     ic(id_f)
-                    # Run queries
+                    # Combine clauses to eliminate duplicates.
                     where_clauses.append(f"{id_f} = '{id_value}'")
-                    
+
                     #q = f'SELECT * FROM "{resource_id}" WHERE {id_f} = \'{id_value}\''
             if len(where_clauses) > 0:
                 fill_bowl(resource_id)
                 q = f'SELECT * FROM "{resource_id}" WHERE {" OR ".join(where_clauses)}'
                 print(q)
                 list_of_dicts += query_resource(site, q, API_key)
-                # Combine results to eliminate duplicates.
-                ic(source['job_code'])
+                ic(job_code)
                 ic(list_of_dicts)
-                if len(list_of_dicts) > 1: 
+                if len(list_of_dicts) > 1:
+                    ic(len(list_of_dicts))
                     ic(list_of_dicts)
-                    print("Combining these results into one (if possible) based on _id fields.")
-                    reduced_list__ids = list(set([d['_id'] for d in list_of_dicts]))
-                    assert len(reduced_list__ids) == len(list_of_dicts)
-                    reduced_list = []
+                    assert 0
 
             # Store the data so it can be tacked onto the fields.csv file.
-            records_by_job_code[source['job_code']] = list_of_dicts
+            #records_by_job_code[job_code] = list_of_dicts
 
-            for job_code, list_of_records in records_by_job_code.items():
-                for record in list_of_records:
-                    for field, value in record.items():
-                        if field not in ['_id', '_geom', '_the_geom_webmercator', '_full_text']:
-                            source['field'] = field
-                            source['value'] = value
-                            rows_with_values.append(dict(source))
+            for record in list_of_dicts:
+                for field, value in record.items():
+                    if field not in ['_id', '_geom', '_the_geom_webmercator', '_full_text']:
+                        source['field'] = field
+                        source['value'] = value
+                        rows_with_values.append(dict(source))
+                        value_by_job_code_and_field[job_code][field] = value
 
-    return rows_with_values
+    return rows_with_values, value_by_job_code_and_field
 
 def generate_housing_project_file_from_multitable_query(job, **kwparameters):
     # Get field name and value (interactively?)
     id_field_name = input("Enter name of property ID field: ")
     id_field_value = input(f"Search for {id_field_name} = ")
 
-    rows_with_values = query_housing_project(id_field_name, id_field_value)
+    rows_with_values, value_by_job_code_and_field = query_housing_project(id_field_name, id_field_value)
     write_to_csv(job.target, rows_with_values)
+    other_path = re.sub('fields', 'ordered_fields', job.target)
+
+    with open('/Users/drw/WPRDC/etl/rocket-etl/output_files/house_cat/fields.csv') as g:
+        reader = csv.DictReader(g)
+        all_rows_with_values = []
+        for row in reader:
+            job_code = row['job_code']
+            value_by_field = value_by_job_code_and_field.get(job_code, defaultdict(None))
+            row['value'] = value_by_field.get(row['field'], None)
+            all_rows_with_values.append(row)
+
+        ic(other_path)
+        write_to_csv(other_path, all_rows_with_values)
 
 from engine.payload.house_cat._parameters import housecat_tango_with_django_package_id #'3f6411c8-d03d-45b2-8225-673841e5c2b3'
 
